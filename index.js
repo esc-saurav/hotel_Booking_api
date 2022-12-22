314fa9461a904e2e16593902f0f92bdfc75927d4
Saurav panthee <sauravpanthiedu@gmail.com>
2022-12-22 13:04:41 +0545

    hotel controller created

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ import roomRoute from "./routes/rooms.js";
 const app = express();
 dotenv.config();
 
+mongoose.set('strictQuery', true);
+
+
 const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO);
@@ -18,13 +21,24 @@ const connect = async () => {
   }
 };
 
-app.use(express.json())
+app.use(express.json());
 
 //middlewhere
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomRoute);
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
 
 mongoose.connection.on("disconnected", () => {
   console.log("Mongo db disconnected");
